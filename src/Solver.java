36018ecc1620cf9513e24d6e82f90ea3b5c54a75
danetteCafe <louis.viot@protonmail.com>
2019-08-10 16:06:19 +0200

    adding statistical studies for tree set based solver

--- src/Solver.java
+++ src/Solver.java
@@ -1,12 +1,12 @@
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
 /**
- * 
+ *
  * @author loux
  * @param <T>
  */
@@ -17,44 +17,44 @@ public abstract class Solver<T extends Item> {
     public Solver() {
         order_ = 0;
     }
-    
+
     public int getOrder() {
         return order_;
     }
 
-    public abstract void sort(User user, List<T> unsortedItems, T[] sortedItems);
+    public abstract void sort(User user, T[] unsortedItems, T[] sortedItems);
 
     public static class FromTreeSet<U extends Item> extends Solver<U> {
 
         @Override
-        public void sort(User user, List<U> unsortedItems, U[] sortedItems) {
-            MyComparator<U> comparator = new MyComparator<>(user);
+        public void sort(User user, U[] unsortedItems, U[] sortedItems) {
+            ComparatorFromUserInput<U> comparator = new ComparatorFromUserInput<>(user);
             Set<U> sortedItemsSet = new TreeSet<>(comparator);
-            Iterator<U> iterator = unsortedItems.iterator();
-            while (iterator.hasNext()) {
-                U item = iterator.next();
-                sortedItemsSet.add(item);
-            }
-            iterator = sortedItemsSet.iterator();
+            sortedItemsSet.addAll(Arrays.asList(unsortedItems));
+            Iterator<U> iterator = sortedItemsSet.iterator();
             int itemIndex = -1;
             while (iterator.hasNext()) {
                 U item = iterator.next();
-                itemIndex ++;
+                itemIndex++;
                 sortedItems[itemIndex] = item;
             }
-            order_ = comparator.order_ - 1;
+            order_ = comparator.getOrder();
         }
 
-        private static class MyComparator<T extends Item> implements Comparator<T> {
+        private static class ComparatorFromUserInput<T extends Item> implements Comparator<T> {
 
             private final User user_;
             private int order_;
 
-            public MyComparator(User user) {
+            public ComparatorFromUserInput(User user) {
                 user_ = user;
                 order_ = 0;
             }
-
+            
+            public int getOrder() {
+                return order_;
+            }
+            
             @Override
             public int compare(T t, T t1) {
                 order_++;
--- src/User.java
+++ src/User.java
@@ -1,10 +1,17 @@
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ *
+ * @author loux
+ * @param <T>
+ */
 public abstract class User <T extends Item> {
     private static final Logger LOG = Logger.getLogger(User.class.getName());
     private final Map<UUID, T> items_;
@@ -26,13 +33,33 @@ public abstract class User <T extends Item> {
         return (items_.containsKey(item.getID()));
     }
     
-    public abstract int compare(T t, T t1);
+    public List<T> getItems() {
+        return new ArrayList<>(items_.values());
+    }
+    
+    public int compare(T item, T item1) {
+        if (!hasItem(item)) {
+            RuntimeException ex = new RuntimeException("User does not have item " + item.getName());
+            LOG.log(Level.SEVERE, ex.getMessage());
+            throw ex;
+        }
+        if (!hasItem(item1)) {
+            RuntimeException ex = new RuntimeException("User does not have item " + item1.getName());
+            LOG.log(Level.SEVERE, ex.getMessage());
+            throw ex;
+        }
+        return (compare_(item, item1));
+    }
+    
+    
+    
+    protected abstract int compare_(T item, T item1);
     
     public static class DummyUser extends User<Item.DummyItem> {
 
         @Override
-        public int compare(Item.DummyItem t, Item.DummyItem t1) {
-            return (t.getItem() - t1.getItem());
+        public int compare_(Item.DummyItem item, Item.DummyItem item1) {
+            return (item.getItem() - item1.getItem());
         }
         
     }
